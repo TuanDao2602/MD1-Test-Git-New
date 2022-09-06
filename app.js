ef4a27ecad8031dae5302bae83582cc45dae60ee
TuanDao2602 <tuanktktcn@gmail.com>
2022-09-06 11:16:22 +0700

    up

---
+++ app.js
@@ -0,0 +1,9 @@
+// for (let i=0 ,j=10;i<=10;i++,j--){
+//     let sum= i+j;
+//     document.write(i+"+"+j+"="+sum+'<br>')
+//
+// }
+
+for(let i=0;i<100;i++){
+    document.write('<hr width='+i+'%>')
+}
